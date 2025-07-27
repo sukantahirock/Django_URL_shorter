b19340b4b763be9c5696adaa85b67a55c0ec60f6
sukantahirock <haridasnag01715511031@gmail.com>
2025-07-27 14:58:09 +0600

    Designed model for db

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import string, random
 
-# Create your models here.
+def generate_short_code():
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=6))
+
+class ShortURL(models.Model):
+    original_url = models.URLField()
+    short_code = models.CharField(max_length=10, unique=True, default=generate_short_code)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.short_code
