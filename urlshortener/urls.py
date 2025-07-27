f998ad8d75818a11f9aea69996f7f4b5807d296f
sukantahirock <haridasnag01715511031@gmail.com>
2025-07-27 14:59:45 +0600

    Fixed project URL

--- urlshortener/urls.py
+++ urlshortener/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from shortener.views import redirect_to_original
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('shortener.urls')),
+    path('<str:code>/', redirect_to_original),
 ]
